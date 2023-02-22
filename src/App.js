421a7e208dd49858419b763ea8cac618849a6676
Dniel Rhde <danielbadenrohde@gmail.com>
2023-02-22 12:16:09 +0100

    Initialised project. Added frontpage header

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p id="title-name">
+          Daniel<br></br>Rohde
+        </p>
+        <p>
+          Fullstack developer<br></br> living in Denmark
+        </p>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
